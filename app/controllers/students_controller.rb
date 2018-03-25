8ce6b04f4508e06546b15e0076f70292b1938b62
Berry  Semexan <bsemexan@gmail.com>
2018-03-25 06:05:39 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_params(:first_name))
+    @student = Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
@@ -24,7 +24,7 @@ class StudentsController < ApplicationController
 
   def update
     @student = Student.find(params[:id])
-    @student.update(student_params(:first_name))
+    @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
