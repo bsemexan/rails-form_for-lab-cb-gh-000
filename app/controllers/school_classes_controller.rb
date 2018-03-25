593b3f7dd7a7da6d7799703a76f864683c12c285
Berry  Semexan <bsemexan@gmail.com>
2018-03-25 05:56:03 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,6 +10,8 @@ class SchoolClassesController < ApplicationController
 
   def create
     @class = SchoolClass.new(class_params(:title,:room_number))
+    @class.save 
+    redirect_to school_class_path(@class)
   end
 
   private
