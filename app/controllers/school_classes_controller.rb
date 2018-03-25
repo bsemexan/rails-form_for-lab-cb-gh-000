7526b67f34692be28a64fefcbb6830376a69f8e9
Berry  Semexan <bsemexan@gmail.com>
2018-03-25 06:06:21 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -24,7 +24,7 @@ class SchoolClassesController < ApplicationController
 
   def update
     @class = SchoolClass.find(params[:id])
-    @class.update(class_params(:title))
+    @class.update(class_params(:title, :room_number))
     redirect_to school_class_path(@class)
   end
 
